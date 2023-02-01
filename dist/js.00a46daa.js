11b53d244e3754fb75f50b03dcf08c575c07a88b
El3aber <abadi444449@gmail.com>
2023-02-01 12:52:19 +0300

    ''

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -37010,7 +37010,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55715" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63235" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
